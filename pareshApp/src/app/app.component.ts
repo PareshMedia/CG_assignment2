26020183becd079f3e857e4e7d48a9c30055ea37
Sapna <sap.awasarmol@gmail.com>
2017-11-16 00:43:13 +0530

    CRUD added

--- pareshApp/src/app/app.component.ts
+++ pareshApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dashboard';
+  showUpdate = false;
+  tempObj = false;
+  dummyData = [{  
+	   "name":"Alex",
+	   "age":"22",
+	   "location":"pune"
+	},{  
+	   "name":"John",
+	   "age":"11",
+	   "location":"goa"
+	},{  
+	   "name":"Noman",
+	   "age":"34",
+	   "location":"Raipur"
+	},{  
+	   "name":"Rakesh",
+	   "age":"25",
+	   "location":"Rigadh"
+	},{  
+	   "name":"Olks",
+	   "age":"54",
+	   "location":"Nima"
+	},{  
+	   "name":"Olex",
+	   "age":"12",
+	   "location":"Pune"
+	},{  
+	   "name":"Raju",
+	   "age":"23",
+	   "location":"Nagpur"
+	},{  
+	   "name":"Rajesh",
+	   "age":"21",
+	   "location":"Jaipur"
+	}
+  ];
+
+  remove = function(obj){
+  		this.dummyData = this.dummyData.filter(item => item !== obj);
+  };
+
+  add = function() {
+		this.dummyData.push({  
+		   "name":this.nameModel,
+		   "age":this.ageModel,
+		   "location":this.locModel
+		});
+		this.locModel = "";
+		this.nameModel = "";
+		this.ageModel = "";
+  };
+
+  edit = function(obj){
+  		this.tempObj = obj;
+  		if(this.showUpdate){
+  			alert("Please update selected first !!");
+  			return false;
+  		}
+  		this.showUpdate = true;
+  		this.nameUpdateModel = obj.name;
+  		this.ageUpdateModel = obj.age;
+  		this.locUpdateModel = obj.location;
+  		this.dummyData = this.dummyData.filter(item => item !== obj);
+  };
+
+  update = function(){
+  		this.dummyData.push({  
+		   "name":this.nameUpdateModel,
+		   "age":this.ageUpdateModel,
+		   "location":this.locUpdateModel
+		});
+		this.nameUpdateModel = "";
+		this.ageUpdateModel = "";
+		this.locUpdateModel = "";
+		this.showUpdate = false;
+  	};
+
+
+  updateCancel = function(){
+  		this.dummyData.push(this.tempObj);
+  		this.showUpdate = false;
+  		this.nameUpdateModel = "";
+		this.ageUpdateModel = "";
+		this.locUpdateModel = "";
+  };
+
+  };
+
+
 }
